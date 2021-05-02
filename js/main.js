f442bd395dfabb41f22b9485bf6d50278e0919cd
Patricio Baeza <Patricio.Baeza8672@gmail.com>
2021-05-01 22:27:07 -0400

    commit 09

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(function(){
 	$('.multiple-items').slick({
 	  infinite: true,
 	  slidesToShow: 3,
-	  slidesToScroll: 3
+	  slidesToScroll: 1,
+	  arrows: false,
+	  autoplay: true,
+	  autoplaySpeed: 2000
 	});
 });
