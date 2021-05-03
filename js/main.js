764533089b2a7f4a8d112ea6e7111f690f14c2ac
Patricio Baeza <Patricio.Baeza8672@gmail.com>
2021-05-02 22:09:01 -0400

    commit 17

--- js/main.js
+++ js/main.js
@@ -13,13 +13,15 @@ $(document).ready(function(){
 			  slidesToShow: 3,
 			  slidesToScroll: 3,
 			  infinite: true,
-			  dots: true
+			  arrows: false,
+			  dots: false
 			}
 		  },
 		  {
 			breakpoint: 600,
 			settings: {
 			  slidesToShow: 2,
+			  arrows: false,
 			  slidesToScroll: 2
 			}
 		  },
@@ -27,12 +29,10 @@ $(document).ready(function(){
 			breakpoint: 480,
 			settings: {
 			  slidesToShow: 1,
+			  arrows: false,
 			  slidesToScroll: 1
 			}
 		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
 		]
 	  });
 });
